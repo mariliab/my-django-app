c9e687626395824536a9bec169ec1501fb7b1736
mariliab <marilia.bognandi@gmail.com>
2020-10-12 20:53:42 +0200

    sending json response to index view instead of template

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+from django.forms.models import model_to_dict
 
 from .models import Choice, Question
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+def index(request):
+    questions = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date').values()
+    questions_list = list(questions)  # important: convert the QuerySet to a list object
+    return JsonResponse(questions_list, safe=False)
 
 class DetailView(generic.DetailView):
-    model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
